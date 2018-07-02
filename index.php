37e4870c3785aa9c271f4ef1e13ebfe131be9327
Pavel <johnconan@mail.ru>
2018-07-02 13:08:28 +0400

    redbean-ready

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+	require "db.php";
+
+// Создание новых записей
+// $course = R::dispense('courses');
+// $course->title = "Курс по верстке";
+// $course->homework = "8";
+// R::store($course);	
+
+// Получение всех записей
+// $courses = R::find('courses');
+// foreach ($courses as $course) {
+// 	echo "ID: " . $course->id . "<br>" ;
+// }
+
+//Получение одного курса
+// $course = R::load('courses', 4);
+// echo "ID: " . $course->id . "<br>";
+
+// удаление записей
+	// $course = R::load('courses', 1);
+	// R::trash($course);
+ ?>
+
